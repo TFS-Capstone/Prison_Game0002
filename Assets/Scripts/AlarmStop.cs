06b947d2de0fe9489c2d0c91d84edb4ed0d1a8b1
GarethAlgar <57628834+GarethAlgar@users.noreply.github.com>
2020-10-28 11:54:16 -0400

    Revert "Got Animation Skeleton set, and Camera System"
    This reverts commit 6aed35b5e4d53a84379cf5be31200d89b175c663, reversing
    changes made to aab532068d61d3e6e5722ea116c7e7f18e28e204.

--- Assets/Scripts/AlarmStop.cs
+++ Assets/Scripts/AlarmStop.cs
@@ -7,7 +7,7 @@ public class AlarmStop : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 5);
+        Destroy(this, 5);
     }
 
     // Update is called once per frame
