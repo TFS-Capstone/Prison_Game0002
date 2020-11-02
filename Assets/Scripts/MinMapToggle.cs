2c050ec9c8b010bc94e182dd94515a5e7e0c22ed
GarethAlgar <57628834+GarethAlgar@users.noreply.github.com>
2020-11-02 12:52:35 -0500

    MiniMapCam centered on Player
    A smaller field of view is obtained from the minimap camera as well as it is now centered on the player at all times.
    
    This can easily be removed should we desire by deleting the update Function of MiniMapToggle and and increasing the camera projection size on the specified miniMap camera

--- Assets/Scripts/MinMapToggle.cs
+++ Assets/Scripts/MinMapToggle.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class MinMapToggle : MonoBehaviour
 {
     public GameObject miniMapCanvas;
+    GameObject miniMapCam;
+    Transform playerPos;
     // Start is called before the first frame update
     void Start()
     {
         miniMapCanvas = GameObject.FindGameObjectWithTag("MiniMap");
         miniMapCanvas.SetActive(false);
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        miniMapCam = GameObject.FindGameObjectWithTag("MiniMapCam");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        miniMapCam.transform.position = new Vector3(playerPos.position.x, miniMapCam.transform.position.y, playerPos.position.z);
     }
 }
